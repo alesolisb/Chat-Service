25f608e42712eaa2b55ce50c3dd8931a40cba8ae
alesolisb <alesolisb01@gmail.com>
2023-08-17 20:02:53 -0600

    GUI básica del cliente

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+
 public class App {
+        
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Ventana_Cliente cliente = new Ventana_Cliente();
+        System.out.println(cliente.getTitle());
+
+        
     }
 }
---
+++ src/Ventana_Cliente.java
@@ -0,0 +1,53 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class Ventana_Cliente extends JFrame {
+    int WIDTH=1000;
+    int HEIGHT=1000;
+    JButton enviar;
+    JTextField mensaje;
+    JPanel panel_inferior;
+    JTextArea chat;
+
+    Ventana_Cliente(){
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setTitle("ChatApp");
+        this.setResizable(false);
+        //this.setSize(WIDTH, HEIGHT);
+        
+        
+
+        enviar = new JButton("Enviar", null);
+
+        mensaje = new JTextField();
+        mensaje.setPreferredSize(new Dimension(600, 40));
+
+        chat= new JTextArea();
+        chat.setBackground(Color.DARK_GRAY);
+        chat.setForeground(Color.WHITE);
+        chat.setPreferredSize(new Dimension(750, 500));
+
+        panel_inferior = new JPanel();
+        panel_inferior.setBackground(Color.LIGHT_GRAY);
+        panel_inferior.setPreferredSize(new Dimension(750, 50));
+
+        this.add(chat, BorderLayout.NORTH);
+        this.add(panel_inferior, BorderLayout.SOUTH);
+        panel_inferior.add(mensaje);
+        panel_inferior.add(enviar); 
+        this.pack();
+        this.setVisible(true);
+
+
+
+
+    }
+}
